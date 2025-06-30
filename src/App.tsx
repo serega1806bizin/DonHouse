53699a93245ad300d18315ad7b25fa179a865deb
serega1806bizin <seregabizin@icloud.com>
2025-06-30 09:56:04 +0300

    Добавил проверку на тип устройтсва

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { adminRoutes } from './app/admin/routes';
 import './main.css';
@@ -5,6 +6,29 @@ import { ConfigProvider } from 'antd';
 import ruRU from 'antd/lib/locale/ru_RU';
 
 export default function App() {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkScreen(); // начальная проверка
+    window.addEventListener('resize', checkScreen);
+
+    return () => window.removeEventListener('resize', checkScreen);
+  }, []);
+
+  if (!isMobile) {
+    return (
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h2 style={{ color: 'white' }}>
+          Извините, это приложение доступно только на мобильных устройствах.
+        </h2>
+      </div>
+    );
+  }
+
   return (
     <ConfigProvider
       locale={ruRU}
